fa789af0acb49f6df67f3c703ca0ef43f2a09e74
Kerry Buckley <kerryjbuckley@gmail.com>
2014-09-20 10:04:48 +0100

    This is how the files ended up afterwards, FWIW

--- files/ThreadTest.java
+++ files/ThreadTest.java
@@ -2,22 +2,22 @@ public class ThreadTest implements Runnable {
   public static void main(String args[]) {
     int i, j;
 
-// create 1000 threads
-      for (i=0; i<=1000; i++) {
-        Thread th = new Thread(new ThreadTest());
-          th.start();
-}
+    // create 1000 threads
+    for (i=0; i<=1000; i++) {
+      Thread th = new Thread(new ThreadTest());
+      th.start();
+    }
 
-  // do some calculation
-  for(i=j=0; i<=100000; i++) {
-    j = j + i
+    // do some calculation
+    for(i=j=0; i<=100000; i++) {
+      j = j + i;
+    }
   }
-}
 
-public void run() {
-try {
-// sleep forever
-Thread.currentThread().sleep(1000000);
-} catch (InterruptedException e) {}
-}
+  public void run() {
+    try {
+      // sleep forever
+      Thread.currentThread().sleep(1000000);
+    } catch (InterruptedException e) {}
+  }
 }
